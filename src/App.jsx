6f0d7b18f7832edc976296f0ac914d7c51e093f8
mmmmmmvk <maximilianvdovkin@gmail.com>
2024-12-08 11:05:56 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,33 @@ import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { Experience } from './components/Experience'
 import { Environment } from '@react-three/drei'
+import Introduction from './components/Introduction'
+import Projectpreview from './components/Projectpreview'
+import Slider from './components/Slider'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-    <Canvas>
-      <Experience />
-      <directionalLight intensity={2} position={[0, 2, 3]}/>
-      <Environment files="/img/studio_hdri.hdr" environmentIntensity={5} />
-    </Canvas>
+    <div>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Canvas>
+          <Experience />
+          <directionalLight intensity={2} position={[0, 2, 3]} />
+          <Environment files="/img/studio_hdri.hdr" environmentIntensity={5} />
+        </Canvas>
+      </div>
+      <div>
+        <section className="canvas"></section>
+        <section className="introduction">
+          <Introduction />
+        </section>
+        <section className="projects-main">
+          <Projectpreview />
+          <Slider />
+        </section>
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { motion } from "motion/react"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Projects from './components/projects.jsx'
 import Introduction from './components/Introduction.jsx'
 import Projectpreview from './components/Projectpreview.jsx'
 import Slider from './components/Slider.jsx'
 
+
 createRoot(document.getElementById('root')).render(
-  <>
-  <div>
-  <section className="canvas">
   <StrictMode>
-    <App/>
+    <div>
+      <Router>
+        <div>
+          <nav className="header">
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </div>
   </StrictMode>
-  </section>
-  <div className="header">
-    <a href="#home">Home</a>
-    <a href="#projects">Projects</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-  </div>
-  <section className="introduction">
-    <Introduction />
-  </section>
-  <section className="projects-main">
-    <Projectpreview />
-    <Slider />
-
-
-  </section>
-  </div>
-  </>
 )
